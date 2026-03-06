7cf0fe2ecd760ac12b3580d6fbf52db207c8d3ce
unknown <pelegartzi@gmail.com>
2026-03-06 15:33:22 +0200

    add side bar component

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,13 +1,24 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import { SidebarProvider, SidebarTrigger } from './components/ui/sidebar';
+import AppSidebar from './components/atoms/app-sidebar';
 
 function App() {
 
   return (
     <>
+    <SidebarProvider defaultOpen={false}>
+      
+        <AppSidebar>
+
+        </AppSidebar>
+        <SidebarTrigger></SidebarTrigger>
+      
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/asd" element={<div>asd</div>} />
       </Routes>
+      </SidebarProvider>
     </>
   )
 }
