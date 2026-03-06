6a4f878c6262f9adfb0ed8ba5e6a49b78cbeb574
Omer Attali <116371591+omerattali25@users.noreply.github.com>
2026-03-06 12:48:41 +0200

    Web/feat/kan 10 add shadcn UI (#1)
    * refactor: changed folder structure
    
    * feat: add example environment configuration and contributing guidelines
    
    * feat: update dependencies and add new components for improved UI

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,12 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
-import './App.css'
+import Home from './pages/Home';
 
 function App() {
 
   return (
     <>
       <Routes>
-        <Route path="/" element={<div>Home</div>} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </>
   )
