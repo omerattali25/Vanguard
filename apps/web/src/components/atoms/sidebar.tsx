88c81454259a7ee23c7a55885386ec1050d57078
unknown <pelegartzi@gmail.com>
2026-03-06 14:43:14 +0200

    added atoms folder

---
+++ apps/web/src/components/atoms/sidebar.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const menu_items = [
+    {
+        title : "Home",
+        url : "/",
+        icon
+    }
+]
+
+const AppSidebar = () => {
+  return (
+    <>
+         
+    </>
+  )
+}
+
+export default AppSidebar
