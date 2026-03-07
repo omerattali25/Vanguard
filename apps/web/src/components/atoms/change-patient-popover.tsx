17ba7982ea0c74188b9ba6e5ffe9170ada35324d
Arielle Gershgoren <ariellegershgoren@h-MacBook-Pro-sl-Arielle.local>
2026-03-07 18:26:54 +0200

    added change patiant popover

---
+++ apps/web/src/components/atoms/change-patient-popover.tsx
@@ -0,0 +1,63 @@
+
+import { Button } from "../ui/button";
+import {
+  PopoverContent,
+  PopoverHeader,
+  PopoverTrigger,
+  Popover,
+  PopoverTitle,
+  PopoverDescription,
+} from "../ui/popover";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
+import type { Machine } from "@/types/machine";
+
+interface ChangePatientPopoverProps {
+  machine: Machine  
+  patients: string[];
+}
+export const ChangePatientPopover: React.FC<ChangePatientPopoverProps> = (
+  props,
+) => {
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button variant="outline" onClick={()=>{}}>
+          החלף מטופל
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent>
+        <PopoverHeader>
+          <PopoverTitle>החלף מטופל </PopoverTitle>
+          <PopoverDescription>
+            בחר מטופל אחר כדי להעביר את מכונת ההנשמה
+          </PopoverDescription>
+        </PopoverHeader>
+        <Select>
+          <SelectTrigger className="w-full max-w-48">
+            <SelectValue placeholder="בחר מטופל"/>
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectLabel>מטופלים</SelectLabel>
+              {
+                props.patients.map(patient=>{
+                  return(
+                    <SelectItem value={patient}>{patient}</SelectItem>
+                  )
+                })
+              }
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+      </PopoverContent>
+    </Popover>
+  );
+};
