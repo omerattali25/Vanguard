f23050e14b317a5d61523f564fe5a7e10d35e39e
Arielle Gershgoren <ariellegershgoren@h-MacBook-Pro-sl-Arielle.local>
2026-03-08 11:06:14 +0200

    removed inport form app

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -2,7 +2,6 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import { SidebarProvider, SidebarTrigger } from './components/ui/sidebar';
 import AppSidebar from './components/atoms/app-sidebar';
-import { AddMachineForm } from './components/atoms/machines/add-machine-form';
 
 function App() {
 
