3d10e1d804350851c37f99b4f2585272725b0cef
unknown <pelegartzi@gmail.com>
2026-03-09 12:58:59 +0200

    add configurations to .example.env

--- services/patients/src/patients/patients.module.ts
+++ services/patients/src/patients/patients.module.ts
@@ -6,7 +6,7 @@ import { Patient } from '../entity/patient.entity';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ConfigModule.forRoot(),TypeOrmModule.forFeature([Patient])],
+  imports: [ConfigModule.forRoot() ,TypeOrmModule.forFeature([Patient])],
   controllers: [PatientsController],
   providers: [PatientsService],
   exports: [PatientsService]
