d9f45722980f032a2785d45f6e35ce73a78ba172
Yotam <yotam.yadin@gmail.com>
2026-03-09 19:28:23 +0200

    added connectivty to redis and processing of data to fit

--- services/recommendation_service/src/app.module.ts
+++ services/recommendation_service/src/app.module.ts
@@ -1,10 +1,14 @@
+import * as dotenv from "dotenv"
+dotenv.config();
+
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RecommendationsModule } from './recommendations/recommendations.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [RecommendationsModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), RecommendationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- services/recommendation_service/src/recommendations/recommendations.controller.ts
+++ services/recommendation_service/src/recommendations/recommendations.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { RecommendationsService } from './recommendations.service';
 
 @Controller('recommendations')
-export class RecommendationsController {}
+export class RecommendationsController {
+  constructor(
+    private readonly recommendationsService: RecommendationsService,
+  ) {}
+  @Get()
+  async getRecommendations() {
+    let raw_data = await this.recommendationsService.getTopRecommendations();
+    
+    const entries = raw_data.reduce(
+      (acc, val, i) => {
+        if (i % 2 === 0) acc[val] = parseFloat(raw_data[i + 1]);
+        return acc;
+      },
+      {} as Record<string, number>,
+    );
+    return entries;
+  }
+}
