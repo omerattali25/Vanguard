735271e23208878fcae246ca6944b0f28eb4fee8
omerattali25 <omerattali25@gmail.com>
2026-03-09 15:53:15 +0200

    refactor: update dependencies and refactor bootstrap logic in patients service

--- services/patients/src/config/database.config.ts
+++ services/patients/src/config/database.config.ts
@@ -1,7 +1,7 @@
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 
 export const db_patients_config: TypeOrmModuleOptions = {
-    type: process.env.DB_TYPE as any || '',
+    type: 'postgres',
     host: process.env.DB_HOST || '',
     port: parseInt(process.env.DB_PORT||"") || 0,
     username: process.env.DB_USERNAME || '',
--- services/patients/src/main.ts
+++ services/patients/src/main.ts
@@ -5,20 +5,19 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
 
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AppModule,
-    {
-      transport: Transport.KAFKA,
-      options: {
-        client: {
-          brokers: [process.env.KAFKA_LISTENING || ""],
-        },
-        consumer: {
-          groupId: process.env.GROUP_ID || "",
-        },
+  const app = await NestFactory.create(AppModule);
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers: [process.env.KAFKA_LISTENING || ""],
       },
-    }
-  );
+      consumer: {
+        groupId: process.env.GROUP_ID || "",
+      },
+    },
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -27,7 +26,8 @@ async function bootstrap() {
     }),
   );
   
-  await app.listen();
+  app.startAllMicroservices();
+  await app.listen(process.env.PORT || 3002);
 }
 bootstrap();
 
