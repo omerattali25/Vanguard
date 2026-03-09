f1c99a8026d509c2493bc0c6d9428fc00b9875f1
omerattali25 <omerattali25@gmail.com>
2026-03-09 13:55:48 +0200

    chore: remove proto package and related files for Vitals service

--- packages/proto/src/vitals/vitals.grpc.ts
+++
@@ -1,4 +0,0 @@
-export const VITALS_PACKAGE_NAME = "vitals";
-export const VITALS_SERVICE_NAME = "VitalsService";
-export const VITALS_CLIENT = Symbol("VITALS_CLIENT");
-export const VITALS_PROTO_PATH = "src/vitals/vitals.proto";
