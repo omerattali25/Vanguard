65889b144db1a81af60bfa63dd8e02b9afaa3d96
Yotam <yotam.yadin@gmail.com>
2026-03-09 19:28:23 +0200

    added connectivty to redis and processing of data to fit

--- services/recommendation_service/src/recommendations/recommendations.module.ts
+++ services/recommendation_service/src/recommendations/recommendations.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { RecommendationsService } from './recommendations/recommendations.service';
 import { RecommendationsService } from './recommendations.service';
 import { RecommendationsController } from './recommendations.controller';
 
 @Module({
   providers: [RecommendationsService],
-  controllers: [RecommendationsController]
+  controllers: [RecommendationsController],
 })
 export class RecommendationsModule {}
--- services/recommendation_service/src/recommendations/recommendations.service.ts
+++ services/recommendation_service/src/recommendations/recommendations.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
-
+import Redis from 'ioredis';
 @Injectable()
-export class RecommendationsService {}
+export class RecommendationsService {
+  private readonly redis;
+  constructor() {
+    this.redis = new Redis({
+      host: process.env.REDIS_HOST || '',
+      port: parseInt(process.env.REDIS_PORT || '1'),
+    });
+  }
+
+  async getTopRecommendations() {
+    const raw = await this.redis.zrevrange('riskIndex', 0, parseInt(process.env.MAX_PATIENTS || '6'), 'WITHSCORES');
+    return raw;
+  }
+}
