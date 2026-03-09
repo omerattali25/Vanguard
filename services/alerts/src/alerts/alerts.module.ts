83d818f67c4bce27484065b5fd7a3775db7c9c12
aviv4671 <avivm484@gmail.com>
2026-03-09 14:12:03 +0200

    bugfix: deleted files I dont use and made everything use env variables

--- services/alerts/src/alerts/alerts.module.ts
+++ services/alerts/src/alerts/alerts.module.ts
@@ -3,7 +3,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Alert } from './entity/alert.entity';
 import { KafkaController } from 'src/kafka/kafka.consumer.controller';
 import { AlertsService } from './alerts.service';
-import { ConfigModule } from '@nestjs/config';
 @Module({
     imports: [TypeOrmModule.forFeature([Alert])],
     controllers: [KafkaController],
