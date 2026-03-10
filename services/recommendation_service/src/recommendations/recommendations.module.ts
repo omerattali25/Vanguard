0ad001d231b4b892f1a0439c7bae63608e680822
Yotam <yotam.yadin@gmail.com>
2026-03-10 17:45:46 +0200

    added typeorm and integration

--- services/recommendation_service/src/recommendations/recommendations.module.ts
+++ services/recommendation_service/src/recommendations/recommendations.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { RecommendationsService } from './recommendations.service';
 import { RecommendationsController } from './recommendations.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Machine } from 'src/entities/machine.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Machine])],
   providers: [RecommendationsService],
   controllers: [RecommendationsController],
 })
--- services/recommendation_service/src/recommendations/recommendations.service.ts
+++ services/recommendation_service/src/recommendations/recommendations.service.ts
@@ -1,13 +1,49 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import Redis from 'ioredis';
+import { Machine } from 'src/entities/machine.entity';
+import { IsNull, Not, Repository } from 'typeorm';
 @Injectable()
 export class RecommendationsService {
   private readonly redis;
-  constructor() {
+  constructor(
+    @InjectRepository(Machine)
+    private machineRepo: Repository<Machine>,
+  ) {
     this.redis = new Redis({
       host: process.env.REDIS_HOST || '',
       port: parseInt(process.env.REDIS_PORT || '1'),
     });
+  }
+
+  private async getPatientsInMachines() {
+    const machines = await this.machineRepo.find({
+      select: ['assigned'],
+      where: {
+        assigned: Not(IsNull()),
+      },
+    });
+    return machines;
+  }
+
+  private makeJsonFromList(raw: string[]) {
+    const entries = raw.reduce(
+      (acc, val, i) => {
+        if (i % 2 === 0) acc[val] = parseFloat(raw[i + 1]);
+        return acc;
+      },
+      {} as Record<string, number>,
+    );
+    return entries;
+  }
+
+  private async filterMachines(patients: Record<string, number>) {
+    const patientsInMachines = await this.getPatientsInMachines();
+    const assignedIds = patientsInMachines.map((m) => m.assigned);
+
+    return Object.fromEntries(
+      Object.entries(patients).filter(([id]) => !assignedIds.includes(id)),
+    );
   }
 
   async getTopRecommendations() {
@@ -17,6 +53,11 @@ export class RecommendationsService {
       parseInt(process.env.MAX_PATIENTS || '5'),
       'WITHSCORES',
     );
-    return raw;
+
+    const entries = this.makeJsonFromList(raw);
+
+    const filteredEntries = await this.filterMachines(entries);
+
+    return filteredEntries;
   }
 }
