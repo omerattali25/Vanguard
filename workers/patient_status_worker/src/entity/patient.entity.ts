e9e0162e6b03adadd80356deb3452012e42a5397
tomer <tomerish88@gmai.com>
2026-03-10 13:26:16 +0200

    Fixed linters

--- workers/patient_status_worker/src/entity/patient.entity.ts
+++ workers/patient_status_worker/src/entity/patient.entity.ts
@@ -1,10 +1,4 @@
-import {
-  Column,
-  CreateDateColumn,
-  Entity,
-  PrimaryColumn,
-  PrimaryGeneratedColumn,
-} from 'typeorm';
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
 
 export enum PatientStatus {
   Stable = 'stable',
