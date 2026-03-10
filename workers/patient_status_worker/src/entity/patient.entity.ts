2959328e113939b9f2a5c46f349b385a0fd41d04
tomer <tomerish88@gmai.com>
2026-03-10 13:03:39 +0200

    Added patient entity copy

---
+++ workers/patient_status_worker/src/entity/patient.entity.ts
@@ -0,0 +1,31 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryColumn,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+export enum PatientStatus {
+  Stable = 'stable',
+  Unstable = 'unstable',
+  Critical = 'critical',
+}
+
+@Entity()
+export class Patient {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  city: string;
+
+  @Column({ type: 'enum', enum: PatientStatus })
+  status: string;
+
+  @CreateDateColumn()
+  registered_at: string;
+}
--- workers/patient_status_worker/src/status-worker/status-worker.service.ts
+++ workers/patient_status_worker/src/status-worker/status-worker.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { Patient } from '../entity/patient.entity';
 
 @Injectable()
 export class StatusWorkerService {
   constructor(
     @InjectRepository(Patient)
-    private patientRepo: Repository<Patient>,
+    private readonly patientRepo: Repository<Patient>,
   ) {}
 
-  consumeVitals(patientVital) {}
+  consumeVitals(patientVital: Patient) {}
 }
