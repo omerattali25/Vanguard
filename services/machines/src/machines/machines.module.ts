2ef9e550250969678336168849b8390856a3e28c
Arielle Gershgoren <ariellegershgoren@h-MacBook-Pro-sl-Arielle.local>
2026-03-11 15:57:53 +0200

    bugfix:updated app module

--- services/machines/src/machines/machines.module.ts
+++ services/machines/src/machines/machines.module.ts
@@ -4,9 +4,10 @@ import { MachinesController } from './machines.controller';
 import { Machine } from './entity/machine.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RedisModule } from 'src/redis/redis.module';
+import { MachineAction } from './entity/machine.action.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Machine]),RedisModule],
+  imports: [TypeOrmModule.forFeature([Machine,MachineAction]),RedisModule],
   controllers: [MachinesController],
   providers: [MachinesService],
   exports: [MachinesService],
