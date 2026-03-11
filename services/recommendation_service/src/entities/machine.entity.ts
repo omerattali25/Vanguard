2fdbe015182bbe1156fc12d0a8092dd15a236f8f
Yotam <yotam.yadin@gmail.com>
2026-03-11 23:00:24 +0200

    changed some types

--- services/recommendation_service/src/entities/machine.entity.ts
+++ services/recommendation_service/src/entities/machine.entity.ts
@@ -1,8 +1,8 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 export enum MachineStatus {
-  AVALIBLE = 'פנוי',
-  USED = 'תפוס',
-  IN_TRANSFER = 'בתנועה',
+  AVALIBLE = 'available',
+  USED = 'used',
+  IN_TRANSFER = 'in_transfer',
 }
 @Entity()
 export class Machine {
@@ -10,7 +10,7 @@ export class Machine {
   name: string;
   @PrimaryGeneratedColumn('uuid')
   id: string;
-  @Column({ default: 'חדר מכונות' })
+  @Column({ default: 'storage' })
   location: string;
   @Column({
     type: 'enum',
@@ -18,6 +18,6 @@ export class Machine {
     default: MachineStatus.AVALIBLE,
   })
   status: MachineStatus;
-  @Column({ nullable: true })
+  @Column({ nullable: true, default: '' })
   assigned: string;
 }
