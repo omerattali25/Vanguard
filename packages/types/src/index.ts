ccd108b047e9c2c427c4a80bf0f1ea077099b2a3
tomer <tomerish88@gmai.com>
2026-03-11 13:15:56 +0200

    feat: Changed env vars

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,3 +1,5 @@
 export * from './types/vitals/vitals.entity';
+export * from './types/vitals/input/patient-vitals.input';
 export * from './types/patients/patient.entity';
-export * from './types/patients/patient.input';
+
+export * from './types/alerts/regular-vitals.config';
