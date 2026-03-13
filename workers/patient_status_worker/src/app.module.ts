ca0b6dd9bfc8250e13568aae2645a37142556dc8
tomer <tomerish88@gmai.com>
2026-03-13 13:16:58 +0200

    Removed usage of unused app controller and service

--- workers/patient_status_worker/src/app.module.ts
+++ workers/patient_status_worker/src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { StatusWorkerModule } from './status-worker/status-worker.module';
 
 @Module({
   imports: [StatusWorkerModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
