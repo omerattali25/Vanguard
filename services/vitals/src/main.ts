522cf6c1a092fb7fa5683eccd985cd43584b8f60
unknown <pelegartzi@gmail.com>
2026-03-13 11:33:09 +0200

    added logger to vital service

--- services/vitals/src/main.ts
+++ services/vitals/src/main.ts
@@ -3,10 +3,33 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { AppConfig } from './config/app';
+import { WinstonModule } from 'nest-winston';
+import * as winston from 'winston';
+import 'winston-daily-rotate-file';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    cors: true
+    cors: true,
+    logger: WinstonModule.createLogger({
+      transports: [
+        new winston.transports.Console(),
+
+        new winston.transports.DailyRotateFile({
+          filename: 'logs/application-%DATE%.log',
+          datePattern: 'YYYY-MM-DD',
+          zippedArchive: false,
+          maxSize: '20m',
+          maxFiles: '14d',
+          level: 'app',
+        }),
+
+        new winston.transports.DailyRotateFile({
+          filename: 'logs/error-%DATE%.log',
+          datePattern: 'YYYY-MM-DD',
+          level: 'error',
+        }),
+      ],
+    }),
   });
 
   app.enableCors({ origin: 'localhost:3000' });
@@ -14,6 +37,7 @@ async function bootstrap() {
   const config = app.get(ConfigService<AppConfig>);
 
   app.connectMicroservice<MicroserviceOptions>({
+
     transport: Transport.KAFKA,
     options: {
       client: {
@@ -28,6 +52,7 @@ async function bootstrap() {
   const port = config.get<number>('env.port', { infer: true });
 
   app.startAllMicroservices();
+  this.logger.log(`Vitals service listening on port ${port}`);
   await app.listen(port);
 }
 bootstrap();
--- services/vitals/src/modules/ingestion/ingestion.service.ts
+++ services/vitals/src/modules/ingestion/ingestion.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { VitalEntity } from '@vanguard/types';
 import { Repository } from 'typeorm';
@@ -7,25 +7,32 @@ import { Repository } from 'typeorm';
 export class IngestionService {
   constructor(@InjectRepository(VitalEntity) private vitalRepository: Repository<VitalEntity>) {}
 
+  private logger = new Logger(IngestionService.name);
+
   async create(payload: VitalEntity): Promise<VitalEntity> {
+    this.logger.log(`Adding vital for patient ${payload.patient_id}`);
     const vital = this.vitalRepository.create(payload);
     return this.vitalRepository.save(vital);
   }
 
   async getVitals(): Promise<VitalEntity[]> {
+    this.logger.log('Fetching all vitals');
     return this.vitalRepository.find();
   }
   
   async getVitalById(id: string): Promise<VitalEntity | null> {
+    this.logger.log(`Fetching vital by ID: ${id}`);
     const vital = await this.vitalRepository.findOneBy({ id });
     
     if (!vital) {
+      this.logger.warn(`Vital with ID ${id} not found`);
       throw new NotFoundException('Vital not found');
     }
     return vital;
   }
   
   async getVitalByPatientId(patientId: string, limit: number = 100): Promise<VitalEntity[]> {
+    this.logger.log(`Fetching vitals for patient ID: ${patientId} with limit: ${limit}`);
     return this.vitalRepository.find({
       where: {
         patient_id: patientId
