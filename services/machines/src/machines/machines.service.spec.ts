e963f9f5ac47a13b8c33450c25778c52e1e32694
ariellegil <151838088+ariellegil@users.noreply.github.com>
2026-03-15 15:01:41 +0200

    Machines service/fix tests=import (#49)
    * bugfix:chnaged import in machine test
    
    * bugfix:fixed test imports
    
    ---------
    
    Co-authored-by: Arielle Gershgoren <ariellegershgoren@h-MacBook-Pro-sl-Arielle.local>

--- services/machines/src/machines/machines.service.spec.ts
+++ services/machines/src/machines/machines.service.spec.ts
@@ -1,11 +1,8 @@
 import { Test } from '@nestjs/testing';
 import { MachinesService } from './machines.service';
 import { getRepositoryToken } from '@nestjs/typeorm';
-import { Machine, MachineStatus } from '@vanguard/types';
 import { Repository } from 'typeorm';
-import { MachineAction, MachineActionType } from './entity/machine.action.entity';
-import {Patient} from '@vanguard/types';
-import {PatientStatus} from '@vanguard/types'
+import {MachineAction,Machine,MachineActionType,MachineStatus,Patient} from '@vanguard/types'
 
 let machineRepo: jest.Mocked<Repository<Machine>>;
 let actionsRepo: jest.Mocked<Repository<MachineAction>>;
