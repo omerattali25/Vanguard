d412e9b03450cdeb0bf58bd5bd49335cbecf7163
unknown <pelegartzi@gmail.com>
2026-03-15 11:22:01 +0200

    add configurations to pachages

--- services/alerts/src/main.ts
+++ services/alerts/src/main.ts
@@ -2,9 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { ValidationPipe } from '@nestjs/common';
-import * as winston from 'winston';
-import 'winston-daily-rotate-file';
-import { WinstonModule } from 'nest-winston';
+import { LoggerConfig } from '@vanguard/configurations';
 
 async function bootstrap() {
 
@@ -20,26 +18,7 @@ async function bootstrap() {
           groupId: String(process.env.KAFKA_GROUP_ID),
         },
       },
-      logger: WinstonModule.createLogger({
-        transports: [
-          new winston.transports.Console(),
-
-          new winston.transports.DailyRotateFile({
-            filename: 'logs/application-%DATE%.log',
-            datePattern: 'YYYY-MM-DD',
-            zippedArchive: false,
-            maxSize: '20m',
-            maxFiles: '14d',
-            level: 'app',
-          }),
-
-          new winston.transports.DailyRotateFile({
-            filename: 'logs/error-%DATE%.log',
-            datePattern: 'YYYY-MM-DD',
-            level: 'error',
-          }),
-        ],
-      })
+      logger: LoggerConfig,
     }
   );
   app.useGlobalPipes(
