3f06965c64d4e15b263bdd2b268f48ad8b3cf9e5
Arielle Gershgoren <ariellegershgoren@h-MacBook-Pro-sl-Arielle.local>
2026-03-15 14:40:15 +0200

    feat:added machine action repo

--- services/analytics/src/analytics/analytics.service.ts
+++ services/analytics/src/analytics/analytics.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { MachineAction } from '@vanguard/types';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class AnalyticsService {}
+export class AnalyticsService {
+  constructor(
+    @InjectRepository(MachineAction)
+    private readonly machineActionRepo: Repository<MachineAction>,
+  ) {}
+}
--- services/machines/src/machines/machines.service.spec.ts
+++ services/machines/src/machines/machines.service.spec.ts
@@ -3,7 +3,7 @@ import { MachinesService } from './machines.service';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Machine, MachineStatus } from '@vanguard/types';
 import { Repository } from 'typeorm';
-import { MachineAction, MachineActionType } from './entity/machine.action.entity';
+import { MachineAction, MachineActionType } from '@vanguard/types';
 import {Patient} from '@vanguard/types';
 import {PatientStatus} from '@vanguard/types'
 
