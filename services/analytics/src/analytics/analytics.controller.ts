0c4e9298b41656088f72ddf819a9c2cb18f48660
tomer <tomerish88@gmai.com>
2026-03-15 21:14:36 +0200

    feat: Added analytics endpoints for all machine actions, and most and least machine-using patient

--- services/analytics/src/analytics/analytics.controller.ts
+++ services/analytics/src/analytics/analytics.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { AnalyticsService } from './analytics.service';
+import { MachineAction, Patient } from '@vanguard/types';
 
 @Controller('analytics')
-export class AnalyticsController {}
+export class AnalyticsController {
+  constructor(private readonly analyticsService: AnalyticsService) {}
+
+  @Get()
+  getAllMachineActions(): Promise<MachineAction[]> {
+    return this.analyticsService.getAllMachineActions();
+  }
+
+  @Get('most-machine-usage')
+  getPatientWithMostMachineUsage(): Promise<Patient | null> {
+    return this.analyticsService.getPatientWithMostMachineUsage();
+  }
+
+  @Get('least-machine-usage')
+  getPatientWithLeastMachineUsage(): Promise<Patient | null> {
+    return this.analyticsService.getPatientWithLeastMachineUsage();
+  }
+}
