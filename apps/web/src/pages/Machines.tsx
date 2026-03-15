cb9b9c0ffd9fe94d5a1f1743bc09da6727200cce
ariellegil <151838088+ariellegil@users.noreply.github.com>
2026-03-15 22:20:34 +0200

    Machines/machines UI (#50)
    * refactor: changed folder structure
    
    * feat: add example environment configuration and contributing guidelines
    
    * added atoms folder
    
    * add side bar component
    
    * feat:added types dir
    
    * refctor:changed file name to machine from machines
    
    * feat:added basic table
    
    * feat:finished basic table
    
    * added change patiant popover
    
    * Web/feat/kan 10 add shadcn UI (#1)
    
    * refactor: changed folder structure
    
    * feat: add example environment configuration and contributing guidelines
    
    * feat: update dependencies and add new components for improved UI
    
    * chore: update contributing guidelines for clarity and structure
    
    * added atoms folder
    
    * add side bar component
    
    * feat:added types dir
    
    * refctor:changed file name to machine from machines
    
    * feat:added basic table
    
    * feat:finished basic table
    
    * added machines folder in atoms
    
    * added machines folder in atoms
    
    * moved machines popover to machines folder
    
    * feat: added machine-form and audit import paths
    
    * Machines UI/change table columes (#12)
    
    * Patients UI/kan 21 add menu sidebar (#6)
    
    * added atoms folder
    
    * add side bar component
    
    * fix sidebar component
    
    * finished sidebar
    
    * remove test routes
    
    * added fileds to table
    
    * added patients table
    
    * add generic status badge
    
    * feat:added status and location to machine table
    
    ---------
    
    Co-authored-by: srehi <105223189+srehi@users.noreply.github.com>
    Co-authored-by: Arielle Gershgoren <ariellegershgoren@h-MacBook-Pro-sl-Arielle.local>
    Co-authored-by: unknown <pelegartzi@gmail.com>
    Co-authored-by: Omer Attali <116371591+omerattali25@users.noreply.github.com>
    Co-authored-by: omerattali25 <omerattali25@gmail.com>
    
    * added machines page
    
    * feat:finished machines ui
    
    * feat:added requests from client to machines page
    
    * refactor: deleted calling api calls from ui
    
    * feat:added setup to add machine request in ui
    
    * refactor:updated machines api
    
    * feat:added update machine to machine table
    
    * feat:added update to machine api
    
    * added  machines to api gateway
    
    * feat:added querys to mechine ui
    
    ---------
    
    Co-authored-by: omerattali25 <omerattali25@gmail.com>
    Co-authored-by: unknown <pelegartzi@gmail.com>
    Co-authored-by: Arielle Gershgoren <ariellegershgoren@h-MacBook-Pro-sl-Arielle.local>
    Co-authored-by: Omer Attali <116371591+omerattali25@users.noreply.github.com>
    Co-authored-by: srehi <105223189+srehi@users.noreply.github.com>

---
+++ apps/web/src/pages/Machines.tsx
@@ -0,0 +1,38 @@
+import { MachinesTable } from "@/components/atoms/machines/machines-table";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useMachines } from "api/machines/machines.query";
+import { Machine } from "@/types/machine";
+import React from "react";
+import { MachineStatus } from "@/types/machine-status";
+
+export const MachinesPage: React.FC = () => {
+  const { data, isPending, error } = useMachines();
+  if (isPending) {
+    return(
+     <div className="flex justify-center mt-10">
+        <div className="w-full md:w-1/2 space-y-4">
+          <Skeleton className="h-8 w-40 mx-auto" />
+          <Skeleton className="h-10 w-full" />
+          <Skeleton className="h-10 w-full" />
+          <Skeleton className="h-10 w-full" />
+          <Skeleton className="h-10 w-full" />
+        </div>
+      </div>
+    )
+  }
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+  console.log(data)
+//   if (data!=undefined) {
+//     return (
+//       <>
+//         <MachinesTable machines={data} />
+//       </>
+//     );
+//   }
+  return(
+    <MachinesTable/>
+  )
+
+};
