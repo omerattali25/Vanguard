eced497832b0499d66d25f77650bb7181450554b
aviv4671 <avivm484@gmail.com>
2026-03-16 15:53:25 +0200

    feat: added patients and patient-status rooms to realtime gateway

--- apps/realtime-gateway/src/pubsub/pubsub.controller.ts
+++ apps/realtime-gateway/src/pubsub/pubsub.controller.ts
@@ -2,6 +2,7 @@ import { Controller } from '@nestjs/common';
 import { EventPattern, Payload } from '@nestjs/microservices';
 import { Alert } from 'src/inputs/alert.input';
 import { Machine } from 'src/inputs/machine.input';
+import { PatientDetails, PatinetStatusUpdate } from 'src/inputs/patient.inputs';
 import { Vitals } from 'src/inputs/vitals.input';
 import { SocketGateway } from 'src/websocket/socket.gateway';
 
@@ -22,5 +23,13 @@ export class PubsubController {
   @EventPattern('machines')
   onMachine(@Payload() machine: Machine) {
     this.socket.emitToRoom(`machines`, 'machines', machine);
+  }
+  @EventPattern('patients')
+  onPatient(@Payload() patient: PatientDetails) {
+    this.socket.emitToRoom(`patients`, 'patients', patient);
+  }
+  @EventPattern('patient-status')
+  onPatientStatus(@Payload() patientStatus: PatinetStatusUpdate) {
+    this.socket.emitToRoom(`patient-status`, 'patient-status', patientStatus);
   }
 }
