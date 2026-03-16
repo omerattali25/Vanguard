3e745025295eff5182dca8441fef104f9987d4df
aviv4671 <avivm484@gmail.com>
2026-03-17 00:03:51 +0200

    bugfix: updated recommendations bug when it doesnt update

--- workers/recommendations/src/kafka/risk.service.ts
+++ workers/recommendations/src/kafka/risk.service.ts
@@ -23,8 +23,9 @@ export class RiskService {
     );
   }
 
-  handleVitals(vitals: Vital) {
+  async handleVitals(vitals: Vital) {
     const risk = this.calculateRisk(vitals);
-    this.redis.zadd('riskIndex', risk, vitals.patient_id);
+
+    const result = await this.redis.zadd('riskIndex', risk, vitals.patient_id);
   }
 }
--- workers/recommendations/src/types/vitals.input.ts
+++ workers/recommendations/src/types/vitals.input.ts
@@ -1,18 +1,7 @@
-import { IsPositive, IsString, IsNumber, IsUUID } from 'class-validator';
-
 export class Vital {
-  @IsUUID()
   id: string;
-
-  @IsString()
   patient_id: string;
-
-  @IsNumber()
-  @IsPositive()
   respiratory_rate: number;
-
-  @IsNumber()
-  @IsPositive()
   spO2: number;
   heart_rate: number;
   body_temperature: number;
