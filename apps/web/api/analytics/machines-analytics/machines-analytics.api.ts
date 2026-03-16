05ea3e55691913fe5de9e179645c45951b69166c
srehi <105223189+srehi@users.noreply.github.com>
2026-03-16 16:48:16 +0200

    Analitycs/analitycs UI (#56)
    * Analitycs UI/kan 65 add generic analitycs page (#46)
    
    * Patients/Patient-status-worker (#37)
    
    * feat: Commit before pull
    
    * feat: ignored IDE-specific files
    
    * Added patient entity copy
    
    * Fixed linters
    
    * Removed async from controller method
    
    * feat: ignored IDE-specific files
    
    * feat: Changed env vars
    
    * Calculated patient status based on the irregular vital range
    
    * Added tests for statuts provider and service
    
    * Removed usage of unused app controller and service
    
    ---------
    
    Co-authored-by: Tomer Ish Shalom <tomer@Tomers-MacBook-Air.local>
    Co-authored-by: tomer <tomerish88@gmai.com>
    
    * add macines analytics page
    
    * add patients analytics page
    
    * finished analytics ui
    
    ---------
    
    Co-authored-by: Tomer <131149094+Tomer136@users.noreply.github.com>
    Co-authored-by: Tomer Ish Shalom <tomer@Tomers-MacBook-Air.local>
    Co-authored-by: tomer <tomerish88@gmai.com>
    
    * finish machine analytics ui
    
    * Update apps/api-gateway/src/modules/analytics/analytics.controller.ts
    
    Co-authored-by: Omer Attali <116371591+omerattali25@users.noreply.github.com>
    
    * Update apps/web/src/pages/analitycs/MachinesAnalyticsPage.tsx
    
    Co-authored-by: Omer Attali <116371591+omerattali25@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Tomer <131149094+Tomer136@users.noreply.github.com>
    Co-authored-by: Tomer Ish Shalom <tomer@Tomers-MacBook-Air.local>
    Co-authored-by: tomer <tomerish88@gmai.com>
    Co-authored-by: Omer Attali <116371591+omerattali25@users.noreply.github.com>

--- apps/web/api/analytics/machines-analytics/machines-analytics.api.ts
+++ apps/web/api/analytics/machines-analytics/machines-analytics.api.ts
@@ -2,6 +2,6 @@ import { api } from "api/axios/axios";
 import { MachineAction } from "types/machine-action";
 
 export async function getMachinesAnalytics(): Promise<MachineAction[]> {
-    const response = await api.get(`/analitycs/machines`)
+    const response = await api.get(`/analitycs/machines`);
     return response.data;
 }
