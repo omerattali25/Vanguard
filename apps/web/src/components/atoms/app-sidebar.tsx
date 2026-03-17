b1b5bdef7f5c9a38c6d525dba12a64e11690433e
ariellegil <151838088+ariellegil@users.noreply.github.com>
2026-03-17 09:29:19 +0200

    Machines/adding change machine status after ttl (#63)
    * bugfix:chnaged import in machine test
    
    * added changing back to previuse status
    
    * added update only correct row
    
    * added you cant connect someone that connected
    
    ---------
    
    Co-authored-by: Arielle Gershgoren <ariellegershgoren@h-MacBook-Pro-sl-Arielle.local>

--- apps/web/src/components/atoms/app-sidebar.tsx
+++ apps/web/src/components/atoms/app-sidebar.tsx
@@ -1,47 +1,56 @@
-import { BookA, Home } from 'lucide-react'
-import React from 'react'
-import { Sidebar, SidebarContent, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '../ui/sidebar'
-import { Link } from 'react-router-dom'
+import { BookA, Home } from "lucide-react";
+import React from "react";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "../ui/sidebar";
+import { Link } from "react-router-dom";
 
 const menu_items = [
-    {
-        title : "Home",
-        url : "/",
-        icon : Home
-    },
-    {
-        title : "books",
-        url : "/asd",
-        icon : BookA
-    }
-]
+  {
+    title: "Home",
+    url: "/",
+    icon: Home,
+  },
+  {
+    title: "books",
+    url: "/asd",
+    icon: BookA,
+  },
+];
 
 const AppSidebar = () => {
   return (
     <>
-         <Sidebar>
-          <SidebarContent>
-             <SidebarGroup>
-              <SidebarGroupLabel>VANGUARD</SidebarGroupLabel>
-              <SidebarGroupContent>
-                <SidebarMenu>
-                  {menu_items.map((item) =>( 
-                    <SidebarMenuItem key={item.title}>
-                      <Link to={item.url}>
+      <Sidebar>
+        <SidebarContent>
+          <SidebarGroup>
+            <SidebarGroupLabel>VANGUARD</SidebarGroupLabel>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                {menu_items.map((item) => (
+                  <SidebarMenuItem key={item.title}>
+                    <Link to={item.url}>
                       <SidebarMenuButton>
-                        <item.icon/>
+                        <item.icon />
                         <span>{item.title}</span>
                       </SidebarMenuButton>
-                      </Link>
-                    </SidebarMenuItem>
-                  ))}
-                </SidebarMenu>
-              </SidebarGroupContent>
-             </SidebarGroup>
-          </SidebarContent>
-         </Sidebar>
+                    </Link>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        </SidebarContent>
+      </Sidebar>
     </>
-  )
-}
+  );
+};
 
-export default AppSidebar
+export default AppSidebar;
--- apps/web/src/pages/Machines.tsx
+++ apps/web/src/pages/Machines.tsx
@@ -9,8 +9,8 @@ import { ChangePatientContextProvider } from "@/contexts/machines/change-patient
 export const MachinesPage: React.FC = () => {
   const { data, isPending, error } = useMachines();
   if (isPending) {
-    return(
-     <div className="flex justify-center mt-10">
+    return (
+      <div className="flex justify-center mt-10">
         <div className="w-full md:w-1/2 space-y-4">
           <Skeleton className="h-8 w-40 mx-auto" />
           <Skeleton className="h-10 w-full" />
@@ -19,16 +19,14 @@ export const MachinesPage: React.FC = () => {
           <Skeleton className="h-10 w-full" />
         </div>
       </div>
-    )
+    );
   }
   if (error) {
     return <div>Error: {error.message}</div>;
   }
-  console.log(data)
-  return(
+  return (
     <ChangePatientContextProvider>
-     <MachinesTable/>
+      <MachinesTable />
     </ChangePatientContextProvider>
-  )
-
+  );
 };
